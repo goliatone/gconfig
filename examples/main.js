6715bbf62695ab70efbb9496d81d8fe735d6f59d
goliatone <burgosemi@gmail.com>
2013-10-30 12:30:42 -0400

    Example: Added use exaple.

--- examples/main.js
+++ examples/main.js
@@ -10,29 +10,35 @@ requirejs.config({
 define(['gconfig', 'jquery'], function (GConfig, $) {
     console.log('Loading');
 	var config = new GConfig();
+	config.use({'log':function(){
+			var args = Array.prototype.slice.call(arguments);
+			args.unshift('GConfig:: ');
+			console.log.apply(console, args);
+		}
+	});
 	var config2 = new GConfig();
 	config2.set('config2', 'just-addeed');
 
-	console.log(config.get('name'));
-	console.log(config.get('baseurl'));
-	console.log(config.get('default-controller'));
+	config.log(config.get('name'));
+	config.log(config.get('baseurl'));
+	config.log(config.get('default-controller'));
 
 	var widget  = {'template':'widget-template'}; 
 	var service = {'serviceUrl':'http://api.example.com/v1/'};
 	console.log('-----');
-	console.log('cf ', config.meta);
-	console.log('serviceUrl ', config.get('serviceUrl', 'note-set'));
+	config.log('cf ', config.meta);
+	config.log('serviceUrl ', config.get('serviceUrl', 'note-set'));
 	config.merge(service);
 	config.merge(widget, 'widget');
-	console.log('serviceUrl ', config.get('serviceUrl', 'note-set'));
-	console.log('widget template ', config.get('template', 'note-set', 'widget'));
-	console.log('cf ', config.meta);
+	config.log('serviceUrl ', config.get('serviceUrl', 'note-set'));
+	config.log('widget template ', config.get('template', 'note-set', 'widget'));
+	config.log('cf ', config.meta);
 	console.log('-----');
-	console.log('cf2 ', config2.meta);
+	config.log('cf2 ', config2.meta);
 	console.log('-----');
 
 	var vo = {};
 	config.configure(vo);
-	console.log('Configured: %o', vo);
-	console.log('Namespace: %o', config.configure({}, 'widget'));
+	config.log('Configured: %o', vo);
+	config.log('Namespace: %o', config.configure({}, 'widget'));
 });
