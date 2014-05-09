c8998832c6c7a98eed3eb8cbd9b5bddb1a8ef572
goliatone <burgosemi@gmail.com>
2014-05-08 21:43:15 -0400

    SPEC: vanity touch.

--- test/test-main.js
+++ test/test-main.js
@@ -6,7 +6,7 @@
 Function.prototype.bind=Function.prototype.bind||function(b){if(typeof this!=="function"){throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");}var a=Array.prototype.slice,f=a.call(arguments,1),e=this,c=function(){},d=function(){return e.apply(this instanceof c?this:b||window,f.concat(a.call(arguments)));};c.prototype=this.prototype;d.prototype=new c();return d;};
 
 var tests = Object.keys(window.__karma__.files).filter(function (file) {
-      return /-spec\.js$/.test(file);
+    return /-spec\.js$/.test(file);
 });
 
 requirejs.config({
