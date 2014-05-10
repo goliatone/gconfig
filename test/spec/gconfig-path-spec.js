48d68a8b9b847347c1524aa996667e3e4f7a6daf
goliatone <burgosemi@gmail.com>
2014-05-10 18:10:05 -0400

    SPEC: Updated path spec.

--- test/spec/gconfig-path-spec.js
+++ test/spec/gconfig-path-spec.js
@@ -23,29 +23,65 @@ define(['gconfig', 'gconfig.path', 'jquery'], function(GConfig, GCPPath, $) {
         'default-controller': 'Controller'
     };
 
-    describe('just checking', function() {
+    describe('GCPPath plugin...', function() {
 
         beforeEach(function() {
-            GCPPath.register(GConfig);
+            GConfig.extend(GCPPath);
             $('head').append(html);
         });
 
-        it('GConfig should have one extra method added by path plugin', function() {
+        it('should have an ID class property', function() {
+            expect(GCPPath.ID).toBeTruthy();
+        });
+
+        it('should have an VERSION class property', function() {
+            expect(GCPPath.VERSION).toBeTruthy();
+        });
+
+        it('should be tracked by GConfig', function() {
+            expect(GConfig.PLUGINS).toHaveProperties(GCPPath.ID);
+            expect(GConfig.PLUGINS[GCPPath.ID]).toMatch(GCPPath.VERSION);
+        });
+
+        it('should have one extra method added by path plugin', function() {
             var config = new GConfig();
             expect(config).toHaveMethods(['resolve']);
         });
 
-        it('GConfig should resolve dot notation paths', function() {
+        it('should resolve dot notation paths', function() {
             var config = new GConfig();
             expect(config.resolve('app.name')).toMatch(config.get('name'));
         });
 
-        it('GConfig should set dot notation paths', function() {
+        it('should set dot notation paths', function() {
             var config = new GConfig();
             config.set('app.test.value', 'NewValue');
-            window.config = config;
-            // expect(config.data.app.test.value).toMatch('NewValue');
+            expect(config.data.app.test.value).toMatch('NewValue');
             expect(config.resolve('app.test.value')).toMatch('NewValue');
+        });
+    });
+
+    describe('GCPPath helper methods', function() {
+        it('setPropertyChain', function() {
+            var source = {},
+                path = 'app.test.value',
+                value = 'NewValue';
+            GCPPath.h.setPropertyChain(source, path, value);
+            expect(source.app.test.value).toMatch(value);
+        });
+
+        it('resolvePropertyChain', function() {
+            var source = {
+                app: {
+                    test: {
+                        value: 'NewValue'
+                    }
+                }
+            };
+            var path = 'app.test.value',
+                expected = 'NewValue',
+                result = GCPPath.h.resolvePropertyChain(source, path);
+            expect(result).toMatch(expected);
         });
     });
 });
