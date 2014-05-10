216c229b0de7d7ee9fa320040d58ddacafe16564
goliatone <burgosemi@gmail.com>
2014-05-10 00:05:16 -0400

    Path: Refactoring, added version. Making helper methods available to unit test.

--- src/gconfig.path.js
+++ src/gconfig.path.js
@@ -8,10 +8,10 @@
  */
 /* jshint strict: false, plusplus: true */
 /*global define: false, require: false, module: false, exports: false */
-(function (root, name, deps, factory) {
+(function(root, name, deps, factory) {
 
     // Node
-    if(typeof deps === 'function') {
+    if (typeof deps === 'function') {
         factory = deps;
         deps = [];
     }
@@ -24,11 +24,14 @@
         define(name.toLowerCase(), deps, factory);
     } else {
         // Browser
-        var d, i = 0, global = root, old = global[name], mod;
-        while((d = deps[i]) !== undefined) deps[i++] = root[d];
+        var d, i = 0,
+            global = root,
+            old = global[name],
+            mod;
+        while ((d = deps[i]) !== undefined) deps[i++] = root[d];
         global[name] = mod = factory.apply(global, deps);
         //Export no 'conflict module', aliases the module.
-        mod.noConflict = function(){
+        mod.noConflict = function() {
             global[name] = old;
             return mod;
         };
@@ -38,27 +41,43 @@
 
 
     /**
-     * Parse query string into object.
-     * @param   {String|undefined} query String to parse
-     *                                   If undefined, location search
-     *                                   is used.
-     * @return  {Object}       Query string object.
+     * Returns the value of a path in dot notation
+     * to a `target`s property.
+     *
+     * @param   {Object} target
+     * @param   {String} path
+     * @param   {Object} defaultValue
+     * @return  {Object}
      * @private
      */
-    var _resolvePropertyChain = function(target, path, defaultValue){
-        if(typeof path === 'string') path = path.split('.');
-        var l = path.length, i = 0, p = '';
-        for (; i < l; ++i ) {
+    var _resolvePropertyChain = function(target, path, defaultValue) {
+        if (typeof path === 'string') path = path.split('.');
+        var l = path.length,
+            i = 0,
+            p = '';
+        for (; i < l; ++i) {
             p = path[i];
-            if ( target.hasOwnProperty( p ) ) target = target[ p ];
+            if (target.hasOwnProperty(p)) target = target[p];
             else return defaultValue;
         }
         return target;
     };
 
-///////////////////////////////////////////////////
-// CONSTRUCTOR
-///////////////////////////////////////////////////
+    var _setPropertyChain = function(target, key, value) {
+        if (!target) throw new Error('WE NEED A VALID TARGET');
+        var keys = key.split('.');
+        key = keys.pop();
+        keys.forEach(function(prop) {
+            if (!target[prop]) target[prop] = {};
+            target = target[prop];
+        });
+
+        target[key] = value;
+    };
+
+    ///////////////////////////////////////////////////
+    // CONSTRUCTOR
+    ///////////////////////////////////////////////////
 
     /**
      * GCPPath constructor
@@ -66,33 +85,48 @@
      * @param  {object} config Configuration object.
      */
     var GCPPath = {};
+    GCPPath.ID = 'GCPPath';
+    GCPPath.VERSION = '0.3.0';
 
-    GCPPath.register = function(GConfig){
-        GConfig.prototype.resolve = function(path, defaultValue){
+    GCPPath.register = function(GConfig) {
+        /*
+         * Keep a reference to the original
+         * `set` method.
+         */
+        var _set = GConfig.prototype.set;
+
+        /**
+         * Gets the value of a property addressed with
+         * dot notation.
+         * @param  {String} key          Configuration key
+         * @param  {Mixed} defaultValue  Default value
+         * @return {Mixed}
+         */
+        GConfig.prototype.resolve = function(path, defaultValue) {
             return _resolvePropertyChain(this.data, path, defaultValue);
         };
 
-        var _set = GConfig.prototype.set;
-
-        GConfig.prototype.set = function(key, value, namespace){
-            if(!key) return this;
-            if(key.indexOf('.') === -1) return _set.call(this, key, value, namespace);
-            //
-            var keys = key.split('.'),
-            //Need to check for namespace, if it does not exist
-                target = namespace ? this.getNamespace(namespace) : this.data;
-
-            key = keys.pop();
-            keys.forEach(function(prop){
-                if(!target[prop]) target[prop] = {};
-                target = target[prop];
-            });
-            target[key] = value;
-
+        /**
+         * Override original set
+         * Set a key value under the given namespace
+         * @param {String} key       Property value
+         * @param {Object} value     Value to be added
+         * @param  {String} namespace Namespace id
+         * @return {GConfig}          Fluid interface.
+         */
+        GConfig.prototype.set = function(key, value, namespace) {
+            if (!key) return this;
+            if (key.indexOf('.') === -1) return _set.call(this, key, value, namespace);
+            if (!this.getNamespace(namespace, this.data)) throw new Error('FUCK IT')
+            _setPropertyChain(this.getNamespace(namespace, this.data), key, value);
             return this;
         };
     };
 
+    /******************************************************
+     * EXPOSE HELPER METHODS FOR UNIT TESTING.
+    /******************************************************/
+    GCPPath.resolvePropertyChain = _resolvePropertyChain;
 
     return GCPPath;
 }));
