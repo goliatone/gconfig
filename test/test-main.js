92249090299182ad6275660f80308c77ca11cdd5
goliatone <burgosemi@gmail.com>
2014-05-10 20:40:49 -0400

    SPEC: Update test main format

--- test/test-main.js
+++ test/test-main.js
@@ -3,10 +3,23 @@
  * PhantomJS does not have a `bind` method :(
  * polyfill'it!
  */
-Function.prototype.bind=Function.prototype.bind||function(b){if(typeof this!=="function"){throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");}var a=Array.prototype.slice,f=a.call(arguments,1),e=this,c=function(){},d=function(){return e.apply(this instanceof c?this:b||window,f.concat(a.call(arguments)));};c.prototype=this.prototype;d.prototype=new c();return d;};
+Function.prototype.bind = Function.prototype.bind || function(b) {
+    if (typeof this !== "function") {
+        throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+    }
+    var a = Array.prototype.slice,
+        f = a.call(arguments, 1),
+        e = this,
+        c = function() {}, d = function() {
+            return e.apply(this instanceof c ? this : b || window, f.concat(a.call(arguments)));
+        };
+    c.prototype = this.prototype;
+    d.prototype = new c();
+    return d;
+};
 
-var tests = Object.keys(window.__karma__.files).filter(function (file) {
-      return /-spec\.js$/.test(file);
+var tests = Object.keys(window.__karma__.files).filter(function(file) {
+    return /-spec\.js$/.test(file);
 });
 
 requirejs.config({
@@ -23,4 +36,3 @@ requirejs.config({
     // start test run, once Require.js is done
     callback: window.__karma__.start
 });
-
